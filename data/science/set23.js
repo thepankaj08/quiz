c43a3ca59935489efd0f2e0522fa489dfa1b1e89
hotdudehvfun <hotdudehvfun@gmail.com>
2022-02-08 15:02:27 +0530

    help

---
+++ data/science/set23.js
@@ -0,0 +1,123 @@
+let set23=
+[
+    {
+        question:`#1. Which of the following statement marks as a difference between plant cell and animal cell?`,
+        options:
+        [
+'(a) Plant cells have cell wall which animal cells do not.',
+'(b) Plant cells do not have vacuole while animal cells do have.',
+'(c) Plant cells have only cell membrane while animal cells have both cell wall as well as cell membrane.',
+            '(d) Plant cells have more plastids while animal cells have few plastids.',
+        ],
+        answer:`Answer: A`,
+        exp:`Explanation: A plant cell also has chloroplast , which afflicts the plant cells colours . An animals cell DOES not have chloroplast . Plant cells have a cell weveryone and a cell membrane Animal cells solely have a cell membrane Plant cells have chloroplasts Animal cell have n't got the chloroplasts Plant cells have huge vacuoles Animal cell have little vacuoles . Plant Cell : Presence of CELLULOSE Animals Cell : Absence of CELLULOSE Plants Cell : Permanent shape be decided by the cell wall ; Shapes is feasible to approximately spherical , box-like or cylindrical Animal Cell : Shapes were different because there is no cell wall Plant Cell : Chloroplasts are present solely in some cell Animal Cell : Absent Plant Cell : Has an enormous permanent vacuole containing cell sap Animal Cell : Small vacuole ; do not contain cell sap.`
+    },
+    {
+        question:`#2. Osmosis is a process by which molecules of a solvent tend to pass through a semipermeable membrane from a less concentrated solution into a more concentrated one. Can you pick out the option among the following which does not belong to this process?`,
+        options:
+        [
+'(a) The movement of water across a semipermeable membrane is affected by the amount of substances dissolved in it.',
+'(b) Membranes are made of organic molecules such as proteins and lipids.',
+'(c) Molecules soluble in organic solvents can easily pass through the membrane.',
+            '(d) Plasma membranes contain chitin sugar in plants.',
+        ],
+        answer:`Answer: D`,
+        exp:`Explanation: Osmosis is the diffusion of a solvent through a differentially permeable membrane. In biological systems, the solvent will usually be water. Osmosis will occur whenever the water concentrations are different on either side of a differentially permeable membrane. Osmosis can be defined as the movement of water molecules from a higher water concentration area to the area of less water concentration through a semipermeable membrane. In other words, it can be defined as the diffusion of water molecules through a semipermeable membrane. It is a special case of diffusion of water (High to low). For example, water in the roots of plants is transported through osmosis. Significance of Osmosis:  Osmosis influences the transport of nutrients and the release of metabolic waste products.  It is responsible for the absorption of water from the soil and conducting it to the upper parts of the plant through the xylem.  It stabilizes the internal environment of a living organism by maintaining the balance between water and intercellular fluid levels.  It maintains the turgidity of cells.  It is a process by which plants maintain their water content despite the constant water loss due to transpiration.  This process controls the cell to cell diffusion of water.  Osmosis induces cell turgor which regulates the movement of plants and plant parts.  Osmosis also controls the dehiscence of fruits and sporangia.  Higher osmotic pressure protects the plants against drought injury.`
+    },
+    {
+        question:`#3. Cell is the structural and functional unit of life. The word cell is derived from the Latin word ‘cellula’ which means “a little room”. Can you name the scientist who coined the term cell?`,
+        options:
+        [
+'(a) Robert Hooke',
+'(b) Anton Von Leeuwenhoek',
+'(c) Robert Brown',
+            '(d) Ernst Haeckel',
+        ],
+        answer:`Answer: A`,
+        exp:`Explanation: A cell is the structural and fundamental unit of life. The study of cells from its basic structure to the functions of every cell organelle is called Cell Biology. Robert Hooke was the first Biologist who discovered cells. All organisms are made up of cells. They may be made up of a single cell (unicellular), or many cells (multicellular). Mycoplasmas are the smallest known cells. Cells are the building blocks of all living beings. They provide structure to the body and convert the nutrients taken from the food into energy.Cells are complex, and their components perform various functions in an organism. They are of different shapes and sizes, pretty much like bricks of the buildings. Our body is made up of cells of different shapes and sizes.Cells are the lowest level of organisation in every life form. From organism to organism, the count of cells may vary. Humans have the number of cells compared to that of bacteria.Cells comprise several cell organelles that perform specialised functions to carry out life processes. Every organelle has a specific structure. The hereditary material of the organisms is also present in the cells. Robert Hooke discovered the cell in 1665. Robert Hooke observed a piece of bottle cork under a compound microscope and noticed minuscule structures that reminded him of small rooms. Consequently, he named these “rooms” as cells. However, his compound microscope had limited magnification, and hence, he could not see any details in the structure. Because of this limitation, Hooke concluded that these were non-living entities. Later Anton Van Leeuwenhoek observed cells under another compound microscope with higher magnification. This time, he had noted that the cells exhibited some form of movement (motility). As a result, Leeuwenhoek concluded that these microscopic entities were “alive.” Eventually, after a host of other observations, these entities were named as animalcules. In 1883, Robert Brown, a Scottish botanist, provided the very first insights into the cell structure. He was able to describe the nucleus present in the cells of orchids. Characteristics of Cells: Following are the various essential characteristics of cells:  Cells provide structure and support to the body of an organism.  The cell interior is organised into different individual organelles surrounded by a separate membrane.  The nucleus(major organelle) holds genetic information necessary for reproduction and cell growth.  Every cell has one nucleus and membrane-bound organelles in the cytoplasm.  Mitochondria, a double membrane-bound organelle is mainly responsible for the energy transactions vital for the survival of the cell.  Lysosomes digest unwanted materials in the cell.  Endoplasmic reticulum plays a significant role in the internal organisation of the cell by synthesising selective molecules and processing, directing and sorting them to their appropriate locations.`
+    },
+    {
+        question:`#4. The proteins and lipids, essential for building the cell membrane, are manufactured by:`,
+        options:
+        [
+'(a) Endoplasmic reticulum',
+'(b) Golgi apparatus',
+'(c) Mitochondria',
+            '(d) Peroxisomes',
+        ],
+        answer:`Answer: A`,
+        exp:`Explanation: Endoplasmic reticulum (ER), in biology, a continuous membrane system that forms a series of flattened sacs within the cytoplasm of eukaryotic cells and serves multiple functions, being important particularly in the synthesis, folding, modification, and transport of proteins . All eukaryotic cells contain an endoplasmic reticulum (ER). In animal cells, the ER usually constitutes more than half of the membranous content of the cell. Differences in certain physical and functional characteristics distinguish the two types of ER, known as rough ER and smooth ER.`
+    },
+    {
+        question:`#5. Tissue is a group of similar kind of cells specialized to perform a particular function in the body. Therefore presence of tissues in a multicellular organism ensures:`,
+        options:
+        [
+'(a) Faster development',
+'(b) Division of labour',
+'(c) Higher reproductive potential',
+            '(d) Body strength',
+        ],
+        answer:`Answer: B`,
+        exp:`Explanation: In simple terms, tissues can be defined as a group of cells with similar shape and function are termed as tissues. They form a cellular organizational level intermediate between the cells and organ system. Organs are then formed by combining the functional groups of multiple tissues.`
+    },
+    {
+        question:`#6. Lysosome is a cytoplasmic organelle containingenzymesthat break down biological polymers. Lysosomes function as the digestive system of the cell. It is also called the suicide bag of the cell because:`,
+        options:
+        [
+'(a) It causes any cell to commit suicide',
+'(b) Its enzymes digest the cell itself',
+'(c) Its enzymes kill surrounding cells',
+            '(d) All of the above',
+        ],
+        answer:`Answer: B`,
+        exp:`Explanation: Lysosomes are an important cell organelle found within eukaryotic animal cells. Due to their peculiar function, they are also known as the “suicide bags” of the cell. The term was coined by Christian de Duve, a Belgian biologist, who discovered it and ultimately got a Nobel Prize in Medicine or Physiology in the year 1974. Lysosome Definition: “Lysosomes are sphere-shaped sacs filled with hydrolytic enzymes that have the capability to break down many types of biomolecules.” In other words, lysosomes are membranous organelles whose specific function is to breakdown cellular wastes and debris by engulfing it with hydrolytic enzymes. Lysosomes work as the waste discarding structures of the cell by processing undesirable materials and degrading them, both from the exterior of the cell and waste constituents inside the cell. But sometimes, the digestive enzymes may end up damaging the lysosomes themselves, and this can cause the cell to die. This is termed as autolysis, where “auto” means “self” and “lysis” means “the disintegration of the cell by the destruction of its cell membrane“. Hence, lysosomes are known as “Suicidal Bags” of the cell.`
+    },
+    {
+        question:`#7. Epithelial tissue always has an exposed outer surface and an inner surface anchored to connective tissue by a thin, noncellular structure called the`,
+        options:
+        [
+'(a) Non-stratified layer',
+'(b) Stratified layer',
+'(c) Basement membrane',
+            '(d) Fibroblast',
+        ],
+        answer:`Answer: C`,
+        exp:`Explanation: Epithelial Tissue: They are formed by cells which cover the external parts of the body organs and lines the organ surfaces such as the surface of the skin, the reproductive tract, the airways, and the inner lining of the digestive tract. Functions of Epithelial Tissue: 1. Play a major role in sensory reception, excretion, filtration and other metabolic activities. 2. Provide mechanical strength and resistance to the underlying cells and tissue. 3. It is involved in the movement of materials through the process of filtration, diffusion and secretion. 4. Protects the internal organs against the invasions of pathogens, toxins, physical trauma, radiation, etc. Epithelial tissues are also involved in secreting hormones, enzymes, mucus and other products from ducts and transporting it to the circulatory system.`
+    },
+    {
+        question:`#8. Connective tissues are the tissues that help to bind or connect other tissues in the body. They have widely spaced cells embedded in a matrix having a variety of proteins, polysaccharides and mineral salts. Can you identify the connective tissues among the following? \n i. Ligament \n ii. Epithelium \n iii. Tendon \n iv. Blood`,
+        options:
+        [
+'(a) Both (i) and (iii)',
+'(b) (i), (ii) and (iii)',
+'(c) (i), (iii) and (iv)',
+            '(d) All (i), (ii), (iii) and (iv)',
+        ],
+        answer:`Answer: C`,
+        exp:`Explanation: Connective Tissues: They are the group of tissues made up of cells separated by non-living material, called as an extracellular matrix. This tissue gives shape to the organs and holds them in place. For example, blood, bone, tendon, adipose, ligament and areolar tissues. There are three types of connective tissue:  Fibrous Connective Tissue.  Fluid Connective Tissue.  Skeletal Connective Tissue. Functions of Connective Tissue The connective tissue gives shape to the organs and holds them in place. It functions as the main supporting tissue of the body. Other important and the major functions of connective tissue in the body are: 1. Insulating. 2. Helps in binding the organs together and provides support. 3. Protects against the invasions of pathogens by their phagocytic activity. 4. Provides shape to the body, conserves body heat and also stores energy. 5. Involved in transporting nutrients, hormones, gases, wastes, and other substances within the body.`
+    },
+    {
+        question:`#9. Tiny pores are found on the surface of the leaves of plants. These pores are called stomata. These stomata surrounded by the kidney shaped guard cells provide many vital functions to the plants. Which of the following functions is not served by the stomata for the plants?`,
+        options:
+        [
+'(a) Exchange of gases, particularly CO2 and O2, with atmosphere',
+'(b) Loss of water in the form of vapours during transpiration',
+'(c) Helps to create pressure for the water to rise upward, by its process of transpiration',
+            '(d) Helps the leaves to carry out the process of photosynthesis',
+        ],
+        answer:`Answer: D`,
+        exp:`Explanation: Answer D`
+    },
+    {
+        question:`#10. Meristemetic tissues are those which help in increasing the length and girth of the plan. Which of the following statements given below is correct about the meristematic tissue?`,
+        options:
+        [
+'(a) It is made up of cells that are incapable of cell division',
+'(b) It is made up of cells that are capable of cell division',
+'(c) It is composed of single type of cells',
+            '(d) It is composed of more than one type of cells',
+        ],
+        answer:`Answer: B`,
+        exp:`Explanation: Meristematic Tissue: They are the group of young cells, which consists of actively dividing cells and leads to increase in length and thickness of the plant. There are different types of meristematic tissues, which are classified on the basis of positions, functions, plane of divisions, origin and development. The three main types of meristematic tissues depending on the occurrence of the meristematic tissue on the plant body are: 1. Apical Meristem. 2. Lateral Meristem. 3. Intercalary Meristem. Functions of Meristematic Tissue 1. It is responsible for the growth of the new organs. 2. Involved in the movement of water and nutrition within the plants. 3. These tissues are responsible for both primary and secondary growth of the plant. 4. It is the outermost tissue, functions by providing protection from mechanical injury. 5. It gives rise to epidermis layer, cortex, endodermis, ground tissue and vascular tissue.`
+    }
+]
