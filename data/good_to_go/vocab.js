90217c4797b5bcb86ee5f2d0192d174a81812d60
thepankaj08 <82504685+thepankaj08@users.noreply.github.com>
2024-04-13 23:15:24 +0530

    Update vocab.js

--- data/good_to_go/vocab.js
+++ data/good_to_go/vocab.js
@@ -1,1 +1,68 @@
+let science =
+[
+    {
+    question: `
+1 Match the List-I with List-II and select the correct answer using the code given below :
+List-I (soil layers) List II (their specifications)
+A. Top soil or A-horizon        1.This layer is soft, porous and can retain more water.
+B. Middle layer or B-horizon    2.It is made up of small lumps of rocks with cracks & crevices.
+C. Third layer or C-horizon     3.Lesser amount of humus but more of minerals.
+D. Bedrock                      4. It is hard and difficult to dig it with a spade.
+`,
+    options: [
+        'A. A-1 B-2 C-3 D-4',
+        'B. A-1 B-3 C-2 D-4',
+        'C. A-3 B-4 C-1 D-2',
+        'D. A-2 B-1 C-4 D-3',
+    ],
+    answer: `Answer: B`,
+    exp: `Answer: B
+Explanation: A vertical section through different layers of the soil
+is called the soil profile. Each layer differs in feel (texture), colour,
+depth and chemical composition. These layers are referred to as
+horizons.
+The uppermost horizon is generally dark in colour as it is rich in
+humus and minerals. The humus makes the soil fertile and provides
+nutrients to growing plants. This layer is generally soft, porous and
+can retain more water. It is called the topsoil or the A-horizon. This
+provides shelter for many living organisms such as worms, rodents,
+moles and beetles. The roots of small plants are embedded entirely
+in the topsoil.
+The next layer has a lesser amount of humus but more of minerals.
+This layer is generally harder and more compact and is called the Bhorizon or the middle layer.
+The third layer is the C-horizon, which is made up of small lumps of
+rocks with cracks and crevices. Below this layer is the bedrock,
+which is hard and difficult to dig with a spade.
+`
+},
+{
+    question: `
+2 Consider the following assertions:
+1. The best topsoil for growing plants is loamy.
+2. Loamy soil is a mixture of sand,clay and slit.
+3. The size of the silt particles is between those of sand and clay.
+4. Sandy soil is suitable for growing wheat, gram and paddy.
+Which of the above assertions is/are correct?
+`,
+    options: [
+        'A. 1 and 2 only',
+        'B. 1, 2 and 3 only',
+        'C. 3 and 4 only',
+        'D. 1, 2, 3 and 4',
 
+    ],
+    answer: `Answer: B`,
+    exp: `Answer: B
+Explanation: The best topsoil for growing plants is loam. Loamy soil
+is a mixture of sand, clay and another type of soil particle known as
+silt. Silt occurs as a deposit in river beds. The size of the silt
+particles is between those of sand and clay.
+The loamy soil also has humus in it. It has the right water holding
+capacity for the growth of plants.
+Clayey soil and loamy soil are suitable for growing wheat, gram and
+paddy, as they have good amount of humus in them; while cotton is
+grown in sandy soil and loamy soil, which facilitate quick draining of
+water.
+`
+}
+  ]
